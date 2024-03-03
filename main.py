90a766dcba7a91e0a549a45a17b8439d3492d7f9
Its.alexito__dev <110727609+itsalexitodev@users.noreply.github.com>
2024-03-03 20:17:52 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,22 @@
-# -*- coding: utf-8 -*-
-
 from juego import *
 
 def main():
-    print("\n Bienvenido al Juego del Piedra, Papel, Tijeras")
-    
-    while True:
-        print("\nQue quieres hacer?\n")
-        print("1. 1vs1")
-        print("2. VS CPU ")
-        print("3. Salir\n")
-        print("Seleccione una opcion (1 - 3):\n")
-        opcion = input()
-
-        if opcion == "1":
-            print("Ingrese el nombre del jugador 1:")
-            player1 = input()
-            
-            print("Ingrese el nombre del jugador 2:")
-            player2 = input()
-            registre_jugadors(player1, player2, score="score", archivo_login="archivo_login")
-        elif opcion == "2":
-            print("Ingrese su nombre:")
-            player1 = input()
-            game_vsCPU(player1)
-        elif opcion == "3":
-            print("Gracias por jugar!")
-            break
-        else:
-            print("Opcion no valida. Por favor, seleccione una opcion del (1 - 3).")
+    os.system('cls' if os.name == 'nt' else 'clear')  # Limpiar pantalla
+    player1 = input("Nombre del jugador 1: ")
+    player2 = input("Nombre del jugador 2: ")
+    players = {player1: {'ganadas': 0, 'perdidas': 0},
+               player2: {'ganadas': 0, 'perdidas': 0}}
+    mostrar_marcador(player1, players[player1], player2, players[player2])
+    print("\nSelecciona el modo de juego:\n")
+    print("1. Jugar contra otro jugador")
+    print("2. Jugar contra la CPU")
+    modo_juego = input("\nIngresa el número del modo de juego: \n")
+    if modo_juego == '1':
+        game_1vs1(players, player1, player2)
+    elif modo_juego == '2':
+        game_vsCPU(player1)
+    else:
+        print("Modo de juego no válido. Por favor selecciona 1 o 2.")
 
 if __name__ == "__main__":
     main()
